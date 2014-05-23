b1401f344f4586ab8a4748c1d31a80852b5402a9
alonso.torres <alonso.torres@kaleidos.net>
2014-05-23 15:08:38 +0200

    Added enum field

--- grails-app/domain/conferences/Speaker.groovy
+++ grails-app/domain/conferences/Speaker.groovy
@@ -5,11 +5,17 @@ class Speaker {
     static hasMany=[talks:Talk]
     String name
     String birthDate
+    SpeakerGender gender = SpeakerGender.FEMALE
 
     static constraints = {
+        gender nullable:true
     }
 
     String toString(){
         return name
     }
+}
+
+enum SpeakerGender {
+    MALE, FEMALE
 }
